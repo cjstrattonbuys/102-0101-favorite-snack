fed7c110e8155d03d54612cc639f82d1770d3256
CJ Buys <cjstrattonbuys@gmail.com>
2022-02-17 09:31:32 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Write your JavaScript below this line
+var name = "CJ Buys";
+var favoriteSnack = "Cheese puffs";
+console.log(
+  "Hi, my name is " +
+    name +
+    " and I wish I had " +
+    favoriteSnack +
+    " right now."
+);
